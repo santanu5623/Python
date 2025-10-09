bf276d249d579e7c0e080a61058d8d4f10d87453
santanu5623 <santanumandal5623@zohomail.in>
2025-10-09 18:33:37 +0530

    Update L5.py

--- Week1/L5.py
+++ Week1/L5.py
@@ -3,3 +3,4 @@ n=str(input("hello, write youre name:"))
 p=str(input("Type your location:"))
 age=int(input("what is your age?:"))
 print("hello",n,"how is the weather in",p,"?","Good to know you are ",age,"years old.")
+print("thanks for you kind attention to this matter"
